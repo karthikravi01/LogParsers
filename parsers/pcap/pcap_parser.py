feb9c235017c153c03cebf6ea97f6ee3aa3cf2a3
karthik Ravi <karthik.ravi@volvocars.com>
2026-01-30 15:53:00 +0100

    Some updates for dlt and basic pcap parser

--- parsers/pcap/pcap_parser.py
+++ parsers/pcap/pcap_parser.py
@@ -1,4 +1,16 @@
 """
 Need to Research Pcap Parsing python modules
+
+1. Scapy (now installed)
+2. other options:
+    - dpkt
+    - pyshark
 """
 
+from scapy.all import rdpcap
+
+packets = rdpcap("./input/net_log_2026-01-20T14_18-17.230613Z.pcap")
+
+for i, pkt in enumerate(packets):
+    print(f"\n===== PACKET {i} =====")
+    pkt.show2()
